b3f615ad5363721a462afa5b6dd8939dc96887bc
Seakon <seakonliu@gmail.com>
2022-02-24 18:15:10 +0800

    first time commit

---
+++ src/test/java/com/example/timemaster/TimeMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
